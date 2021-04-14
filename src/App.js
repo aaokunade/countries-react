dcae5a89a17485639913abc6e0e8f9848b21c4e1
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-04-14 21:41:17 +0100

    data and AllCountriesFiles imported

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import AllCountryFiles from './components/allCountryFiles';
+import Data from './Data.json';
 
 function App() {
+  console.log(Data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AllCountryFiles />
     </div>
   );
 }
