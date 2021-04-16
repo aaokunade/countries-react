1c62ed860765cc26589e3297760863756feff5f1
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-04-16 20:10:57 +0100

    added the handleSelect function

--- src/components/allCountryFiles.js
+++ src/components/allCountryFiles.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import {React, useState} from 'react';
 // import './App.css';
 import AllCountries from './allCountries.js';
 import Data from '../Data.json';
 import SearchBar from './searchBox.js';
+import RegionSelect from './regionSelect.js';
 
 
 const AllCountryFiles = () => {
+    const [countryName, setCountryName] = useState(Data);
+    const [region, setRegion] = useState(Data)
+    const handleSearch = (event) => {
+        const searched = event.target.value.toLowerCase();
+        // console.log(searched);
+        // searched = searched.toLowerCase();
+        const filteredSearch = Data.filter((obj) => {
+            if((obj["name"].toLowerCase().includes(searched)) || (obj["capital"].toLowerCase().includes(searched))) {
+                return obj;
+            }
+        })
+        console.log(filteredSearch);
+        setCountryName(filteredSearch)
+    }
+
+    const handleSelect = (event) => {
+        const selected = event.target.value.toLowerCase()
+        const filteredSelected = Data.filter((obj) => {
+            if (obj["region"].toLowerCase().includes(selected)){
+                return obj;
+            }
+        })
+        setCountryName(filteredSelected);
+    }
+
 return(  
 <div> 
-<SearchBar />
-<AllCountries allData = {Data}/>
+<div className = "regionsearch">
+<SearchBar handleSearch = {handleSearch}/>
+<RegionSelect allData = {countryName} handleSelect = {handleSelect}/>
+</div>
+<AllCountries allData = {countryName}/>
 </div>
 )
 }
