b7d6c22855a24f3c5fff7357b165933edbb49b1a
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-04-17 00:15:25 +0100

    added the regions empty array to take in the regions only if not added

--- src/components/regionSelect.js
+++ src/components/regionSelect.js
@@ -1,11 +1,24 @@
 const RegionSelect = (props) => {
+    const regions = [];
     return (
         <div>
             <select onChange = {props.handleSelect}>
-                {props.allData.map((obj) => {
-                    return <option>{obj["region"]}</option> 
+            <option>All Regions</option>
+                {props.allData.map((obj, index) => {
+                    if(!(regions.includes(obj["region"]))){
+                        regions.push(obj["region"]);
+                    }         
                 }
                 )}
+
+                     {regions.map((region, index)=>{
+                         if(region === ""){
+                             region = "antarctica"
+                         }
+                        return <option key = {index}>{region}</option>
+                
+                    })}
+            
                 
             </select>
         </div>
