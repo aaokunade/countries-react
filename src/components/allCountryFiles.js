23229373b3223faf73111aee2a1b47aa0929872d
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-04-17 00:13:53 +0100

    removed commented lines

--- src/components/allCountryFiles.js
+++ src/components/allCountryFiles.js
@@ -1,5 +1,4 @@
 import {React, useState} from 'react';
-// import './App.css';
 import AllCountries from './allCountries.js';
 import Data from '../Data.json';
 import SearchBar from './searchBox.js';
@@ -8,35 +7,37 @@ import RegionSelect from './regionSelect.js';
 
 const AllCountryFiles = () => {
     const [countryName, setCountryName] = useState(Data);
-    const [region, setRegion] = useState(Data)
     const handleSearch = (event) => {
-        const searched = event.target.value.toLowerCase();
-        // console.log(searched);
-        // searched = searched.toLowerCase();
-        const filteredSearch = Data.filter((obj) => {
+    const searched = event.target.value.toLowerCase();
+          const filteredSearch = Data.filter((obj) => {
             if((obj["name"].toLowerCase().includes(searched)) || (obj["capital"].toLowerCase().includes(searched))) {
                 return obj;
             }
         })
-        console.log(filteredSearch);
         setCountryName(filteredSearch)
     }
 
     const handleSelect = (event) => {
-        const selected = event.target.value.toLowerCase()
+        const selected = event.target.value
+        if(selected === "All Regions"){
+            setCountryName(Data)
+        } else{
+        console.log(selected);
         const filteredSelected = Data.filter((obj) => {
-            if (obj["region"].toLowerCase().includes(selected)){
+             if (obj["region"].includes(selected)){
                 return obj;
-            }
+            }             
         })
         setCountryName(filteredSelected);
+    }
+         
     }
 
 return(  
 <div> 
 <div className = "regionsearch">
 <SearchBar handleSearch = {handleSearch}/>
-<RegionSelect allData = {countryName} handleSelect = {handleSelect}/>
+<RegionSelect allData = {Data} handleSelect = {handleSelect}/>
 </div>
 <AllCountries allData = {countryName}/>
 </div>
