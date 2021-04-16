0287e97fc8401169bf5b6827d41021ea9e519a36
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-04-16 20:11:48 +0100

    added SearchBar component

--- src/components/searchBox.js
+++ src/components/searchBox.js
@@ -1,6 +1,11 @@
-const SearchBar = () => {
-    return <input type = "text" placeholder = "your search here" />
+import AllCountries from "./allCountries"
 
+const SearchBar = (props) => {
+    return     <div>
+    <input onChange = {props.handleSearch} type = "text" placeholder = "your search here" />
+    {/* <AllCountries > */}
+    </div>
+    
 }
 
 export default SearchBar
